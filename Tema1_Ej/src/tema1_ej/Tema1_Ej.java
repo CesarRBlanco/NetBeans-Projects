7eb7623de53dfe1eb7d8df3d57ef46352f842cf0
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-02 18:29:26 +0200

    18:30 - [02/10/19]

--- Tema1_Ej/src/tema1_ej/Tema1_Ej.java
+++ Tema1_Ej/src/tema1_ej/Tema1_Ej.java
@@ -11,6 +11,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,170 +22,244 @@ import java.util.logging.Logger;
  */
 public class Tema1_Ej {
 
-//	public void ejercicio2(File path) {
-//		File[] FList = path.listFiles();
-//
-//		for (File f : FList) {
-//			System.out.println(f.getAbsolutePath());
-//			if (f.isDirectory()) {
-//				File[] FList2 = f.listFiles();
-//				for (File f2 : FList2) {
-//					System.out.println(f2.getAbsolutePath());
-//				}
-//			}
-//		}
-//	}
-//	public void ejercicio3(File fPath, char letra) throws IOException {
-//		int cont = 0;
-//		try (FileReader fRead = new FileReader(fPath)) {
-//
-//			int i;
-//			while ((i = fRead.read()) != -1) {
-//				System.out.print((char) i);
-//				if ((char) i == letra) {
-//					cont++;
-//				}
-//			}
-//		} catch (IOException x) {
-//			System.err.format("IOException: %s%n", x);
-//		}
-//		System.out.println();
-//		System.out.println(cont);
-//	}       
-//	public void ejercicio4(File fPath) throws IOException {
-//		ArrayList<Abecedario> abc=new ArrayList<>();
-//		try (FileReader fRead = new FileReader(fPath)) {
-//			int i;
-//			char ch;
-//			while((i=fRead.read()) != -1){
-//				ch=(char)i;
-//				
-//				if (abc.contains(new Abecedario(ch))) {
-//					abc.get(abc.indexOf(new Abecedario(ch))).suma();
-//					
-//				}else {
-//				Abecedario caracter = new Abecedario(ch);
-//				abc.add(caracter);
-//				}
-//			}
-//		}
-//			finally {
-//				int max=0;
-//				char maxChar = '-';
-//		for(Abecedario letra:abc) {
-//			if(letra.getCont()>=max) {
-//				max=letra.getCont();
-//				maxChar=letra.getLetra();
-//			}
-//		}
-//		System.out.println("El caracter mas repetido es "+maxChar);
-//			}
-//	public class Abecedario {
-//		char letra = '-';
-//		int cont = 1;
-//
-//		public Abecedario(char letra) {
-//			this.letra = letra;
-//
-//		}
-//
-//		public void suma() {
-//			this.cont++;
-//		}
-//
-//		public char getLetra() {
-//			return this.letra;
-//		}
-//
-//		public int getCont() {
-//			return this.cont;
-//		}
-//
-//		@Override
-//		public boolean equals(Object obj) {
-//			return this.letra == ((Abecedario) obj).getLetra();
-//		}
-//
-//	}
-//	public void ejercicio5(File fPath, String fragmento) {
-//		Scanner sc = null;
-//		String frase;
-//		int cont=0;
-//		try (FileReader fRead = new FileReader(fPath)) {
-//			sc = new Scanner(fPath);
-//			while (sc.hasNext()) {
-//			cont++;
-//				frase = sc.nextLine();
-//				if ((frase.toLowerCase()).contains(fragmento.toLowerCase())) {
-//					System.out.println("Linea "+cont+" | "+frase);
-//				}
-//			}
-//		} catch (IOException x) {
-//			System.err.println("Error en el archivo");
-//		}
+    public void ejercicio2(File path) {
+        File[] FList = path.listFiles();
+
+        for (File f : FList) {
+            System.out.println(f.getAbsolutePath());
+            if (f.isDirectory()) {
+                File[] FList2 = f.listFiles();
+                for (File f2 : FList2) {
+                    System.out.println(f2.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    public int ejercicio3(File fPath, char letra)  {
+        int cont = 0;
+        try (FileReader fRead = new FileReader(fPath)) {
+
+            int i;
+            while ((i = fRead.read()) != -1) {
+                System.out.print((char) i);
+                if ((char) i == letra) {
+                    cont++;
+                }
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+            cont=-1;
+        }
+        System.out.println();
+        System.out.println(cont);
+        return cont;
+    }
+
+    public void ejercicio4(File fPath) throws IOException {
+        ArrayList<Abecedario> abc = new ArrayList<>();
+        try (FileReader fRead = new FileReader(fPath)) {
+            int i;
+            char ch;
+            Abecedario aux;
+            while ((i = fRead.read()) != -1) {
+                ch = (char) i;
+                if (abc.contains(new Abecedario(ch))) {
+                    abc.get(abc.indexOf(new Abecedario(ch))).suma();
+
+                } else {
+                    Abecedario caracter = new Abecedario(ch);
+                    abc.add(caracter);
+                }
+            }
+            
+//            while ((i = fRead.read()) != -1) {
+//                aux=new Abecedario((char)i);
+//                if (abc.contains(aux)) {
+//                    abc.get(abc.indexOf(aux)).suma();
+//                } else {
+//                    abc.add(aux);
+//                }
+//            }
+            
+            
+        } finally {
+            int max = 0;
+            char maxChar = '-';
+            for (Abecedario letra : abc) {
+                if (letra.getCont() >= max) {
+                    max = letra.getCont();
+                    maxChar = letra.getLetra();
+                }
+            }
+            System.out.println("El caracter mas repetido es " + maxChar);
+        }
+    }
+	
+
+
+     class Abecedario {
+
+        char letra = '-';
+        int cont = 1;
+
+        public Abecedario(char letra) {
+            this.letra = letra;
+
+        }
+
+        public void suma() {
+            this.cont++;
+        }
+
+        public char getLetra() {
+            return this.letra;
+        }
+
+        public int getCont() {
+            return this.cont;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this.letra == ((Abecedario) obj).getLetra();
+        }
+
+    }
+
+    public void ejercicio5(File fPath, String fragmento) {
+       
+        String frase;
+        int cont = 0;
+        try (Scanner sc = new Scanner(fPath)) {
+              while (sc.hasNext()) {
+                cont++;
+                frase = sc.nextLine();
+                if ((frase.toLowerCase()).contains(fragmento.toLowerCase())) {
+                    System.out.println("Linea " + cont + " | " + frase);
+                }
+            }
+        } catch (IOException x) {
+            System.err.println("Error en el archivo");
+        }
+    }
     
-//    public void ejercicio6(File fPath, int nDiv, boolean nOl) {
-//        if (nOl == true) {
-//            divCaracter(fPath, nDiv);
-//        } else {
-//            divLinea(fPath, nDiv);
-//        }
-//    }
-//    public void divCaracter(File fPath, int nDiv) {
-//        char[] buffer = new char[nDiv];
-//        int i, cont = 1;
-//        try (FileReader fin = new FileReader(fPath)) {
-//            while ((i = fin.read(buffer)) != -1) {
-//                try (FileWriter fout = new FileWriter(fPath.getAbsolutePath().replace(".txt", " ") + cont + ".txt")) {
-//                   try{
-//                    fout.write(buffer, 0, i);
-//                   }catch(Exception e){
-//                       fout.write("");
-//                   }
-//                    cont++;
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            
-//        }
-//    }
-//    public void divLinea(File fPath, int nDiv) {
-//        int cont = 0;
-//        try (Scanner sc = new Scanner(fPath)) {
-//            while (sc.hasNext()) {
-//
-//                try (PrintWriter fich = new PrintWriter(fPath.getAbsolutePath().replace(".txt", " ") + cont + ".txt")) {
-//                    for (int i = 0; i < nDiv; i++) {
-//                 try{
-//                            fich.println(sc.nextLine());
-//                 }catch(Exception e){
-//                     fich.println("");
-//                 } 
-//                    }
-//                    cont++;
-//                }
-//
-//            }
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(Tema1_Ej.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//    }
-//    public void unirFich(String[] array) {
-//        try (PrintWriter fich = new PrintWriter("C:\\Users\\Zer0\\Desktop\\fusion.txt")) {
-//            for (String array1 : array) {
-//                File fichi = new File(array1);
-//                try (Scanner sc = new Scanner(fichi)) {
-//                    while (sc.hasNext()) {
-//                        fich.println(sc.nextLine());
-//                    }
-//                }
-//            }
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(Tema1_Ej.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
+
+    public void ejercicio6(File fPath, int nDiv, boolean nOl) {
+        if (nOl == true) {
+            divCaracter(fPath, nDiv);
+        } else {
+            divLinea(fPath, nDiv);
+        }
+    }
+
+    public void divCaracter(File fPath, int nDiv) {
+        char[] buffer = new char[nDiv];
+        int i, cont = 1;
+        try (FileReader fin = new FileReader(fPath)) {
+            while ((i = fin.read(buffer)) != -1) {
+                try (FileWriter fout = new FileWriter(fPath.getAbsolutePath().replace(".txt", " ") + cont + ".txt")) {
+                    try {
+                        fout.write(buffer, 0, i);
+                    } catch (Exception e) {
+                        fout.write("");
+                    }
+                    cont++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void divLinea(File fPath, int nDiv) {
+        int cont = 0;
+        try (Scanner sc = new Scanner(fPath)) {
+            while (sc.hasNext()) {
+
+                try (PrintWriter fich = new PrintWriter(fPath.getAbsolutePath().replace(".txt", " ") + cont + ".txt")) {
+                    for (int i = 0; i < nDiv; i++) {
+                        try {
+                            fich.println(sc.nextLine());
+                        } catch (Exception e) {
+                            fich.println("");
+                        }
+                    }
+                    cont++;
+                }
+
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Tema1_Ej.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void unirFich(String[] array, String ficheroSalida) {
+        try (PrintWriter fich = new PrintWriter("C:\\Users\\Zer0\\Desktop\\fusion.txt")) {
+            for (String array1 : array) {
+                File fichi = new File(array1);
+                try (Scanner sc = new Scanner(fichi)) {
+                    while (sc.hasNext()) {
+                        fich.println(sc.nextLine());
+                    }
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Tema1_Ej.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void ejercicio7(File fPath, char opcion) {
+        switch (opcion) {
+            case 'n':
+                opcionN(fPath);
+                break;
+            case 'A':
+                opcionA();
+                break;
+            case 'D':
+                opcionD();
+                break;
+            case 'a':
+                opcionAM();
+                break;
+            case 'd':
+                opcionDM();
+                break;
+        }
+    }
+
+    public void opcionN(File fPath) {
+        int cont = 0;
+        try (Scanner sc = new Scanner(fPath)) {
+            while (sc.hasNext()) {
+                System.out.println(cont);
+                cont++;
+            }
+            sc.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Tema1_Ej.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void opcionA() {
+
+    }
+
+    public void opcionD() {
+
+    }
+
+    public void opcionAM() {
+
+    }
+
+    public void opcionDM() {
+
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
@@ -195,20 +270,22 @@ public class Tema1_Ej {
         arrayFich[0] = "C:\\Users\\Zer0\\Desktop\\test_file 0.txt";
         arrayFich[1] = "C:\\Users\\Zer0\\Desktop\\test_file 1.txt";
         File fPath = new File("C:\\Users\\Zer0\\Desktop\\test_file.txt");
+        File f = new File("C:\\Users\\Zer0\\Desktop");
+        char opcion = 'n';
 
-//        File f = new File("C:\\Users\\Zer0\\Desktop");
-//        Ejercicios ej2 = new Ejercicios();
-//        ej2.ejercicio2(f);
-//        Ejercicios ej3 = new Ejercicios();
+        Tema1_Ej ej2 = new Tema1_Ej();
+        ej2.ejercicio2(f);
+//        Tema1_Ej ej3 = new Tema1_Ej();
 //        char letra = 'a';
 //        ej3.ejercicio3(fPath, letra);
-//        Ejercicios ej4 = new Ejercicios();
+//        Tema1_Ej ej4 = new Tema1_Ej();
 //        ej4.ejercicio4(fPath);
-//        Ejercicios ej5 = new Ejercicios();
+//        Tema1_Ej ej5 = new Tema1_Ej();
 //        ej5.ejercicio5(fPath, fragmento);
 //        Tema1_Ej ej6 = new Tema1_Ej();
 //        ej6.ejercicio6(fPath, nDiv, nOl);
 //        ej6.unirFich(arrayFich);
-
+//        Tema1_Ej ej7 = new Tema1_Ej();
+//        ej7.ejercicio7(fPath, opcion);
     }
 }
