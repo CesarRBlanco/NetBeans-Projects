5ba0d38470e88ead9a33807a0f5e636ce4d07b18
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-07 10:41:47 +0200

    10:40 - [07/10/19]

--- PHP/PhpTests/index.php
+++ PHP/PhpTests/index.php
@@ -18,33 +18,71 @@ and open the template in the editor.
                 echo $tabla, "*", $cont, "=", $tabla * $cont, "<br/>";
             }
         }
+
         multiply();
 
         function squareDraw() {
             $side = 5;
             for ($cont = 0; $cont < $side; $cont++) {
-                      echo "<br/>"; 
-          
-                
+                echo "<br/>";
+
+
                 for ($cont2 = 0; $cont2 < $side; $cont2++) {
                     echo "*  ";
-              
                 }
             }
         }
+
         squareDraw();
-        
-//        function piramidDraw(){
-//            $base=5;
-//            for($cont=0;$cont<$base;$cont++){
-//                
-////            for($cont2=;$cont2<$base;$cont2++){
-//                
-//            }    
-//            }
-//        }
-        
+        echo "<br/>";
+
+        function piramidDraw() {
+            $base = 5;
+
+            for ($cont = 1; $cont < $base + 1; $cont++) {
+                echo "<br/>";
+
+                for ($cont3 = $base; $cont3 > $cont; $cont3--) {
+                    echo "&nbsp";
+                }
+
+                for ($cont2 = 0; $cont2 < $cont; $cont2++) {
+                    echo "*";
+                }
+            }
+        }
+
         piramidDraw();
+
+        function piramidHollow() {
+            $base = 5;
+
+            for ($cont = 1; $cont < $base + 1; $cont++) {
+
+                for ($cont3 = $base; $cont3 > $cont; $cont3--) {
+                    echo "&nbsp";
+                }
+                for ($cont2 = 0; $cont2 < $cont; $cont2++) {
+                    if ($cont <= 2) {
+                        echo"*";
+                    }
+                    if ($cont >= 3 && ($cont2 == 0 || $cont2 == $cont - 1)) {
+                        echo"*";
+                    } else {
+                        echo"&nbsp&nbsp";
+                    }
+                    if ($cont == $base) {
+                        for ($cont2 = 0; $cont2 < $cont-1; $cont2++) {
+                            echo"*";
+                        }
+                    }
+                }
+                echo "<br/>";
+            }
+        }
+
+        echo "<br/>";
+        piramidHollow();
         ?>
     </body>
 </html>
