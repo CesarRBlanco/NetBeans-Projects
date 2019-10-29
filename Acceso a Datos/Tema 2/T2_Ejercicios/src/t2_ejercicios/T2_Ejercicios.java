21df0e93a75b24311ef595d041dc0e975886f933
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-10-29 09:34:50 +0100

    [008 A]

--- Acceso a Datos/Tema 2/T2_Ejercicios/src/t2_ejercicios/T2_Ejercicios.java
+++ Acceso a Datos/Tema 2/T2_Ejercicios/src/t2_ejercicios/T2_Ejercicios.java
@@ -19,7 +19,7 @@ import org.w3c.dom.NodeList;
  */
 public class T2_Ejercicios {
 
-    //Creacion del arbol, no tocar.
+//Creacion del arbol, no tocar.
     public Document crearArbol(String ruta) {
         Document doc = null;
         try {
@@ -33,25 +33,32 @@ public class T2_Ejercicios {
         return doc;
     }
 
-    //Metodo para acceder a los datos del XML
+//[Revisar]   
+//Metodo para acceder a los datos del XML
     public void getDato(Node doc, String tag) {
         //Conseguir los nodos que tengan el mismo tag que el que se pasa como parametro
         NodeList titulos = ((Element) doc).getElementsByTagName(tag);
-        NodeList padre;
+        Element padre;
         NamedNodeMap atrib;
         Node atribu;
+        int cont = 0;
+
         for (int i = 0; i < titulos.getLength(); i++) {
-            if (titulos.item(i).getParentNode().equals("pelicula")) {
-                padre = (NodeList) titulos.item(i).getParentNode();
-                
-                atrib = padre.item(i).getAttributes();
-                atribu = atrib.item(i);
+            if (titulos.item(i).getParentNode().getNodeName().equals("pelicula")) {
+                padre = (Element) titulos.item(i).getParentNode();
+                padre.getElementsByTagName(tag);
+                atrib = padre.getAttributes();
+                atribu = atrib.item(1);
                 System.out.println(atribu.getNodeValue());
             }
-            System.out.println(titulos.item(i).getFirstChild().getNodeValue());
+            if (tag.equals("nombre")) {
+                cont++;
+                System.out.println(cont);
+            }
         }
     }
 
+//Llamada al metodo superior para conseguir los datos deseados
     public void datosPeliculas(Document doc) {
         NodeList nombreDirector = doc.getElementsByTagName("pelicula");
         for (int i = 0; i < nombreDirector.getLength(); i++) {
@@ -61,34 +68,90 @@ public class T2_Ejercicios {
             getDato(nombreDirector.item(i), "nombre");
             getDato(nombreDirector.item(i), "apellido");
             System.out.println("-------------------");
-
         }
-//        NodeList nombreDirector = doc.getElementsByTagName("nombre");
-//        int arraysize=nombreDirector.getLength();
-//        String[] nombre=new String[arraysize];
-//        String[] apellidos=new String[arraysize];
-//        for (int j = 0; j < nombreDirector.getLength(); j++) {
-//            nombre[j] = nombreDirector.item(j).getFirstChild().getNodeValue();
-//        }
-//        NodeList apellidosDirector = doc.getElementsByTagName("apellido");
-//        for (int j2 = 0; j2 < apellidosDirector.getLength(); j2++) {
-//            apellidos[j2] = apellidosDirector.item(j2).getFirstChild().getNodeValue();
-//        }
-//
-////        mostrarTitulos(doc);
-//        for (int i = 0; i < nombre.length; i++) {
-//        System.out.println("nombre: " + nombre[i] + "\napellido:" + apellidos[i]);
-//        System.out.println("-------------------");
-//        }
-
     }
 
+
+
+//[Buena suerte entendiendo esto otra vez, campeon]
+    public void masDirectores(Document doc, int n) {
+        NodeList nombreDirector = doc.getElementsByTagName("pelicula");
+        boolean key;
+        for (int i = 0; i < nombreDirector.getLength(); i++) {
+
+            key = getDatoDirec(nombreDirector.item(i), "nombre", n);
+            if (key == true) {
+                getDatoDirec(nombreDirector.item(i), "titulo", n);
+            }
+        }
+    }
+
+    public boolean getDatoDirec(Node doc, String tag, int n) {
+        //Conseguir los nodos que tengan el mismo tag que el que se pasa como parametro
+        NodeList titulos = ((Element) doc).getElementsByTagName(tag);
+        int cont = 0;
+        boolean key = false;
+        for (int i = 0; i < titulos.getLength(); i++) {
+            if (tag.equals("nombre")) {
+                cont++;
+                if (cont == n) {
+                    key = true;
+                }
+            } else {
+                System.out.println(titulos.item(i).getFirstChild().getNodeValue());
+            }
+        }
+        return key;
+    }
+
+    
+    
+    
+    
+    
+    
+    //Llamada al metodo superior para conseguir los datos deseados
+    public void getGeneros(Document doc) {
+         NodeList nombreDirector = doc.getElementsByTagName("pelicula");
+        for (int i = 0; i < nombreDirector.getLength(); i++) {
+
+            getDato(nombreDirector.item(i), "titulo");
+            System.out.println("-------------------");
+        }
+    }
+
+    
+    
+    
+    //Conseguir cuantos generos hay y sus nombres
+    public void generos(Document doc,String tag) {
+     //Conseguir los nodos que tengan el mismo tag que el que se pasa como parametro
+        NodeList titulos = ((Element) doc).getElementsByTagName(tag);
+        Element padre;
+        NamedNodeMap atrib;
+        Node atribu;
+        int cont = 0;
+
+        for (int i = 0; i < titulos.getLength(); i++) {
+            if (titulos.item(i).getParentNode().getNodeName().equals("pelicula")) {
+                padre = (Element) titulos.item(i).getParentNode();
+                padre.getElementsByTagName(tag);
+                atrib = padre.getAttributes();
+                atribu = atrib.item(1);
+                System.out.println(atribu.getNodeValue());
+            }
+        }
+    }
+    
+    
     public static void main(String[] args) {
         String ruta = "C:\\Users\\Zer0\\Desktop\\peliculas.xml";
         Document doc = null;
         T2_Ejercicios gestorArbol = new T2_Ejercicios();
         doc = gestorArbol.crearArbol(ruta);
-        gestorArbol.datosPeliculas(doc);
-    }
+//        gestorArbol.datosPeliculas(doc);
+//        gestorArbol.masDirectores(doc, 2);
+        gestorArbol.getGeneros(doc);
 
+    }
 }
