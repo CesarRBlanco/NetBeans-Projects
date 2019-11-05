280e95b105b110c737cad6e170570bd458df889f
Cesar (Clase) <white.h0und.f0x@gmail.com>
2019-11-05 10:36:12 +0100

    [011 A]

--- Acceso a Datos/Tema 2/T2_Ejercicios/src/t2_ejercicios/T2_Ejercicios.java
+++ Acceso a Datos/Tema 2/T2_Ejercicios/src/t2_ejercicios/T2_Ejercicios.java
@@ -5,6 +5,8 @@
  */
 package t2_ejercicios;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,6 +16,10 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
 
 /**
  *
@@ -144,14 +150,99 @@ public class T2_Ejercicios {
         }
     }
 
-    public static void main(String[] args) {
+    public void añadirAtributo(Document doc, String atributoNodo, String atributoValor, String titulo) {
+        NodeList peliculas = doc.getElementsByTagName("titulo");
+        Element padre;
+        NamedNodeMap atrib;
+        Node atribu;
+        int cont = 0;
+        for (int j = 0; j < peliculas.getLength(); j++) {
+            if (peliculas.item(j).getFirstChild().getNodeValue().equals(titulo)) {
+                if (peliculas.item(j).getParentNode().getNodeName().equals("pelicula")) {
+                    padre = (Element) peliculas.item(j).getParentNode();
+                    padre.getElementsByTagName("titulo");
+                    atrib = padre.getAttributes();
+                    for (int i = 0; i < atrib.getLength(); i++) {
+                        atribu = atrib.item(i);
+                        if (atribu.getNodeName().equals(atributoNodo)) {
+                            cont--;
+                        } else {
+//                            System.out.println(atribu.getNodeValue());
+                            cont++;
+                        }
+                    }
+
+                    if (cont == atrib.getLength()) {
+                        System.out.printf("Voy a agregar %s: %s", atributoNodo, atributoValor);
+                        padre.setAttribute(atributoNodo, atributoValor);
+                    }
+                }
+            }
+        }
+    }
+
+    public void borrarAtributo(Document doc, String atributoNodo, String titulo) {
+        NodeList peliculas = doc.getElementsByTagName("titulo");
+        Element padre;
+        NamedNodeMap atrib;
+        Node atribu;
+        int cont = 0;
+        for (int j = 0; j < peliculas.getLength(); j++) {
+            if (peliculas.item(j).getFirstChild().getNodeValue().equals(titulo)) {
+                if (peliculas.item(j).getParentNode().getNodeName().equals("pelicula")) {
+                    padre = (Element) peliculas.item(j).getParentNode();
+                    padre.getElementsByTagName("titulo");
+                    atrib = padre.getAttributes();
+                    for (int i = 0; i < atrib.getLength(); i++) {
+                        atribu = atrib.item(i);
+                        if (atribu.getNodeName().equals(atributoNodo)) {
+                            padre.removeAttribute(atributoNodo);
+                            System.out.println("Borrado con exito");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void añadirPelicula(Document doc, String rutaSalida) throws ClassNotFoundException, InstantiationException, IllegalAccessException, FileNotFoundException {
+
+Anadir la pelicula al arbol, el resto ya lo graba en nuevo archivo
+        
+
+// Añadir al fichero DOM Nuevo
+        DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+        DOMImplementationLS ls = (DOMImplementationLS) registry.getDOMImplementation("XML 3.0 LS 3.0");
+// Se crea un destino vacio
+        LSOutput output = ls.createLSOutput();
+        output.setEncoding("UTF-8");
+// Se establece el flujo de salida
+        output.setByteStream(new FileOutputStream(rutaSalida));
+//output.setByteStream(System.out);
+// Permite escribir un documento DOM en XML
+        LSSerializer serializer = ls.createLSSerializer();
+// Se establecen las propiedades del serializador
+        serializer.setNewLine("\r\n");;
+        serializer.getDomConfig().setParameter("format-pretty-print", true);
+// Se escribe el documento ya sea en un fichero o en una cadena de texto
+        serializer.write(doc, output);
+// String xmlCad=serializer.writeToString(document);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, FileNotFoundException {
         String ruta = "C:\\Users\\Zer0\\Desktop\\peliculas.xml";
+        String rutaSalida = "C:\\Users\\Zer0\\Desktop\\newMovies.xml";
         Document doc = null;
         T2_Ejercicios gestorArbol = new T2_Ejercicios();
         doc = gestorArbol.crearArbol(ruta);
+        String titulo = "Matrix";
+        String atributoNodo = "presupuesto";
+        String atributovalor = "300.000.000 $";
 //        gestorArbol.datosPeliculas(doc);
 //        gestorArbol.masDirectores(doc, 2);
 //        gestorArbol.getGeneros(doc);
-
+//        gestorArbol.añadirAtributo(doc, atributoNodo, atributovalor, titulo);
+//        gestorArbol.borrarAtributo(doc, atributoNodo, titulo);
+        gestorArbol.añadirPelicula(doc, rutaSalida);
     }
 }
