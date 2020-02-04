829f879f2e4c4a4354463c3be0cb645d107d5492
Cesar <white.h0und.f0x@gmail.com>
2020-02-04 12:29:11 +0100

    [024 A]

--- MariaDB-Exercises/src/mariadb/exercises/MariaDBExercises.java
+++ MariaDB-Exercises/src/mariadb/exercises/MariaDBExercises.java
@@ -7,6 +7,7 @@ package mariadb.exercises;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,6 +15,7 @@ import java.sql.Statement;
 public class MariaDBExercises {
 
     private Connection conexion;
+    private PreparedStatement ps = null;
 
     public void abrirConexion(String bd, String servidor, String usuario, String password) {
         try {
@@ -31,6 +33,14 @@ public class MariaDBExercises {
         }
     }
 
+    public void cerrarConexion() {
+        try {
+            this.conexion.close();
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexión: " + e.getLocalizedMessage());
+        }
+    }
+
     public void ejemplo(String bd) throws SQLException {
         String query = "select * from aulas"; // Consulta a ejecutar
         abrirConexion("add", "localhost", "root", "");
@@ -45,33 +55,91 @@ public class MariaDBExercises {
         cerrarConexion(); // Se cierra la conexión
     }
 
-        public void consultaAlumnos(String bd) throws SQLException {
-        String query = "select * from alumnos where nombre like ? Phillip ?"; // Consulta a ejecutar
+    public void consultaAlumnosPatronNombre(String patron) throws SQLException {
+        String query = "SELECT nombre,cant FROM alumnos,(SELECT count(nombre) AS cant FROM alumnos WHERE nombre LIKE ?) AS subCon WHERE nombre LIKE ?;";
         abrirConexion("add", "localhost", "root", "");
         Statement stmt = this.conexion.createStatement();
-        ResultSet rs = stmt.executeQuery(query); // Se ejecuta la consulta
-        while (rs.next()) { // Mientras queden filas en rs (el método next devuelve true) recorremos las filas
-            System.out.println(rs.getInt(1) + "\t"
-                    + // Se obtiene datos en función del número de columna
-                    rs.getString("nombreAula") + "\t" + rs.getInt("puestos")); // o de su nombre
+        if (this.ps == null) {
+            this.ps = this.conexion.prepareStatement(query);
         }
-        stmt.close(); // Se cierra el Statement
-        cerrarConexion(); // Se cierra la conexión
+        ps.setString(1, "%" + patron + "%");
+        ps.setString(2, "%" + patron + "%");
+        ResultSet resultado = ps.executeQuery();
+        while (resultado.next()) {
+            System.out.println("\t" + resultado.getString("nombre"));
+            if (resultado.isLast()) {
+
+                System.out.println("\t -Cantidad: " + resultado.getString("cant"));
+            }
+
+        }
+        stmt.close();
+        cerrarConexion();
     }
-    
-    
-    public void cerrarConexion() {
+
+    public void insertarAlumno(String codigo, String nombre, String apellido, String altura, String aula) {
         try {
-            this.conexion.close();
+            Statement sta = this.conexion.createStatement();
+            String query = String.format("INSERT INTO alumnos VALUES (%s,'%s','%s',%s,%s)", codigo, nombre, apellido, altura, aula);
+            int filasAfectadas = sta.executeUpdate(query);
+            System.out.println("Filas insertadas: " + filasAfectadas);
+            sta.close();
         } catch (SQLException e) {
-            System.out.println("Error al cerrar la conexión: " + e.getLocalizedMessage());
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
         }
     }
 
+    public void insertarAsignatura(String codigo, String asignatura) {
+        try {
+            Statement sta = this.conexion.createStatement();
+            String query = String.format("INSERT INTO asignaturas VALUES (%s,'%s')", codigo, asignatura);
+            int filasAfectadas = sta.executeUpdate(query);
+            System.out.println("Filas insertadas: " + filasAfectadas);
+            sta.close();
+        } catch (SQLException e) {
+            System.out.println("Se ha producido un error: " + e.getLocalizedMessage());
+        }
+    }
+
+    public void consultaAlumnosAprobados() throws SQLException {
+        String query = " SELECT distinct alumnos.nombre, asignaturas.NOMBRE as AsiNombre,notas.NOTA FROM alumnos,asignaturas JOIN notas WHERE alumnos.codigo=notas.alumno AND asignaturas.COD=notas.asignatura AND notas.NOTA >=5";
+        abrirConexion("add", "localhost", "root", "");
+        Statement stmt = this.conexion.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            System.out.println("\t" + rs.getString("nombre") + " - " + rs.getString("AsiNombre") + " - " + rs.getString("nota"));
+        }
+        stmt.close();
+        cerrarConexion();
+    }
+
+    public void consultaAlumnoPatronesCon(String nombre,String altura) throws SQLException {
+       String query = "SELECT nombre,altura FROM alumnos WHERE nombre LIKE ? and altura>?;";
+        abrirConexion("add", "localhost", "root", "");
+        Statement stmt = this.conexion.createStatement();
+        if (this.ps == null) {
+            this.ps = this.conexion.prepareStatement(query);
+        }
+        ps.setString(1, "%" + nombre + "%");
+        ps.setString(2, "%" + altura + "%");
+        ResultSet resultado = ps.executeQuery();
+        while (resultado.next()) {
+            System.out.println("\t" + resultado.getString("nombre")+" "+resultado.getString("altura"));
+      
+        }
+        stmt.close();
+        cerrarConexion();
+    }
+
     public static void main(String[] args) throws SQLException {
-        MariaDBExercises mdb=new MariaDBExercises();
-        mdb.abrirConexion("add", "127.0.0.1", "root", "");
-        mdb.consultaAlumnos("add");
+        MariaDBExercises mdb = new MariaDBExercises();
+//        mdb.abrirConexion("add", "127.0.0.1", "root", "");
+//        mdb.consultaAlumnosPatronNombre("h");
+//        mdb.insertarAlumno("10", "Natalie", "Paquette", "175", "20");
+//        mdb.insertarAsignatura("9", "Algo");
+//        mdb.consultaAlumnosAprobados();
+mdb.consultaAlumnoPatronesCon("h", "150");
+
     }
 
 }
